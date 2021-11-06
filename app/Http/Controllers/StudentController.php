fd497e333e360069c2957ca1e35c2afe4c7cec66
akbarsatrio <akbarsatrio@outlook.co.id>
2021-11-06 10:51:12 +0700

    udate student

---
+++ app/Http/Controllers/StudentController.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Student;
+
+class StudentController extends Controller {
+    //Index
+    public function index($id = NULL) {
+
+        $student = $id != NULL ? Student::where('id', $id)->get() : Student::all();
+
+        return response()->json([
+            'message' => 'Get all data students',
+            'data' => $student
+        ], 200);
+    }
+
+    public function store(Request $request) {
+        $store_data = [
+            'nama' => $request->nama,
+            'nim' => $request->nim,
+            'email' => $request->email,
+            'jurusan' => $request->jurusan,
+        ];
+
+        $student = Student::create($store_data);
+
+        return response()->json([
+            'message' => 'Student is created succesfully',
+            'data' => $student
+        ], 201);
+    }
+
+    public function update(Request $request, $id) {
+
+        $get_current_data = Student::where('id', $id)->get();
+
+        $store_data = [
+            'nama' => $request->nama??$get_current_data[0]->nama,
+            'nim' => $request->nim??$get_current_data[0]->nim,
+            'email' => $request->email??$get_current_data[0]->email,
+            'jurusan' => $request->jurusan??$get_current_data[0]->jurusan,
+        ];
+
+        Student::where('id', $id)
+                ->update($store_data);
+
+        return response()->json([
+            'message' => "Update data sutdent success with id {$id}",
+            'data' => Student::where('id', $id)->get()
+        ], 200);
+    }
+
+    public function destroy($id) {
+        Student::destroy($id);
+
+        return response()->json([
+            'message' => "Student with id {$id} has been deleted",
+        ], 200);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\AnimalController;
+use App\Http\Controllers\StudentController;
 
 /*
 |--------------------------------------------------------------------------
@@ -26,4 +27,14 @@ Route::post('/animals', [AnimalController::class, 'store']);
 
 Route::put('/animals/{id}', [AnimalController::class, 'update']);
 
-Route::delete('/animals/{destroy}', [AnimalController::class, 'destroy']);
+Route::delete('/animals/{id}', [AnimalController::class, 'destroy']);
+
+
+// Students API
+Route::get('/student/{id}', [StudentController::class, 'index']);
+
+Route::post('/student', [StudentController::class, 'store']);
+
+Route::put('/student/{id}', [StudentController::class, 'update']);
+
+Route::delete('/student/{id}', [StudentController::class, 'destroy']);
